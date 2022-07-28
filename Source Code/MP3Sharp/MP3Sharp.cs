24a6824dadc04afd49bdcfda6f26aee842a808e8
CheeZeDark <110032482+CheeZeDark@users.noreply.github.com>
2022-07-28 13:06:17 +0300

    MP3Sharp

---
+++ Source Code/MP3Sharp/MP3Sharp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+
+namespace MP3Sharp
+{
+    public class InitMP3Sharp
+    {
+        public IWavePlayer player;
+        public AudioFileReader audioFile;
+        public void PlayMP3(string filemp3)
+        {
+            player = new WaveOut();
+            audioFile = new AudioFileReader(filemp3);
+            player.Init(audioFile);
+            player.Play();
+        }
+        public void StopMP3()
+        {
+            player.Stop();
+            audioFile.Dispose();
+            player.Dispose();
+        }
+    }
+}
